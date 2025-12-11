e8ffa5f50a46f0cbcf1a1991e69be123f5b638df
juuuno-coder <your-actual-email@example.com>
2025-12-11 13:55:17 +0900

    Fix: Explicitly run tailwindcss:build in render-build.sh to ensure styles are deployed

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,7 +3,10 @@
 set -o errexit
 
 bundle install
+# Build Tailwind CSS
+bundle exec rails tailwindcss:build
+
+# Precompile assets
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
-
