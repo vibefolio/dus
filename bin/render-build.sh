4c1bb07ffa391c9dff00304b417e3e6817cd38eb
juuuno-coder <your-actual-email@example.com>
2025-12-11 14:16:30 +0900

    Fix: Inject Tailwind CDN as layout failsafe to guarantee correct styling

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,10 +1,6 @@
-#!/usr/bin/env bash
-# exit on error
 set -o errexit
 
 bundle install
-# Build Tailwind CSS
-bundle exec rails tailwindcss:build
 
 # Precompile assets
 bundle exec rails assets:precompile
