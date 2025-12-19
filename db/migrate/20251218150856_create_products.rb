4924943159d85363ec0070cca99dd20011f5fba7
juuuno-coder <your-actual-email@example.com>
2025-12-20 01:30:58 +0900

    Fix migration failure by adding if_not_exists: true

--- db/migrate/20251218150856_create_products.rb
+++ db/migrate/20251218150856_create_products.rb
@@ -1,6 +1,6 @@
 class CreateProducts < ActiveRecord::Migration[8.1]
   def change
-    create_table :products do |t|
+    create_table :products, if_not_exists: true do |t|
       t.string :name
       t.text :description
       t.integer :price
