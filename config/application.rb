f949545178f05404dcaee5c738a6d7ac25266e63
admin <admin@adminui-MacBookPro.local>
2026-01-11 20:16:09 +0900

    Fix build error: Disable DB initialization during assets precompile

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module DlabWebsite
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Prevent DB connection during assets precompile for database-less builds
+    config.assets.initialize_on_precompile = false
   end
 end
