53c1fe611eb203c2420c13a59d684b381372bb4f
admin <admin@adminui-MacBookPro.local>
2026-01-20 15:49:52 +0900

    Disable CSRF origin check to fix 422 error on production login

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,7 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
+  config.action_controller.forgery_protection_origin_check = false
 
   # Skip http-to-https redirect for the default health check endpoint.
   # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
